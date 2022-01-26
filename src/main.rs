bf7e8e04a65fdb2cac3ca3c87439e27c3af5a484
cyanolupus <cyanolupus@gmail.com>
2022-01-26 17:02:17 +0900

    refactoring & add WIP status

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,10 @@ struct Unit {
 }
 
 struct User {
-    units: Vec<Unit>,
+    units_a: Vec<Unit>,
+    units_b: Vec<Unit>,
+    units_c: Vec<Unit>,
+    units_c0: Vec<Unit>,
     gpa: f32,
     gps: f32,
     units_num: f32,
@@ -73,11 +76,14 @@ fn strec2unit(strec: csv::StringRecord) -> Unit {
 fn create_user(csv: String) -> User {
     let mut rdr = csv::Reader::from_reader(csv.as_bytes());
     let mut units: Vec<Unit> = Vec::new();
+    let mut units_a: Vec<Unit> = Vec::new();
+    let mut units_b: Vec<Unit> = Vec::new();
+    let mut units_c: Vec<Unit> = Vec::new();
+    let mut units_c0: Vec<Unit> = Vec::new();
+
     for result in rdr.records() {
         units.push(strec2unit(result.unwrap()));
     }
-
-    let units_cl: Vec<Unit> = units.clone();
 
     let mut gps: f32 = 0.0;
     let mut unum: f32 = 0.0;
@@ -86,12 +92,24 @@ fn create_user(csv: String) -> User {
             gps += unit.unit_num * unit.grade_num;
             unum += unit.unit_num;
         }
+        if unit.unit_group == "A" {
+            units_a.push(unit);
+        } else if unit.unit_group == "B" {
+            units_b.push(unit);
+        } else if unit.unit_group == "C" {
+            units_c.push(unit);
+        } else if unit.unit_group == "C0" {
+            units_c0.push(unit);
+        }
     }
 
     User {
-        units: units_cl,
+        units_a: units_a,
+        units_b: units_b,
+        units_c: units_c,
+        units_c0: units_c0,
         gpa: gps / unum,
-        gps,
+        gps: gps,
         units_num: unum,
     }
 }
@@ -123,26 +141,27 @@ fn check_req(units: Vec<Unit>, reqs: Vec<String>, group: &str) -> Vec<Unit> {
 }
 
 fn check(user: User) -> i32 {
-    println!("start checking {}'s graduation possibility", user.units[0].student_name);
+    println!("start checking {}'s graduation possibility", user.units_a[0].student_name);
 
     let a_req = make_requirement(vec!["主専攻実験A","主専攻実験B","卒業研究A","卒業研究B","専門語学A","専門語学B"]);
     let b_req = make_requirement(vec!["線形代数A","線形代数B","微分積分A","微分積分B","情報数学A","専門英語基礎","プログラミング入門","コンピュータとプログラミング","データ構造とアルゴリズム","データ構造とアルゴリズム実験","論理回路","論理回路実験"]);
     let c_req = make_requirement(vec!["フレッシュマン・セミナー","学問への誘い","English Reading Skills I","English Reading Skills II","English Presentation Skills I","English Presentation Skills II","情報リテラシー(講義)","情報リテラシー(演習)","データサイエンス"]);
 
-    let mut units = check_req(user.units, a_req, "専門    科目");
-
+    let mut units = check_req(user.units_a, a_req, "専門    科目");
     let mut countn0: f32 = 0.0;
     let mut countn: f32 = 0.0;
-    while units.iter().any(|x| x.unit_group == "A" && x.grade_num > -2.0) {
-        let unitsbkt2: Vec<Unit> = units.clone();
-        let unit = unitsbkt2.iter().find(|x| x.unit_group == "A" && x.grade_num > -2.0).unwrap();
-        println!("専門    科目: {:>2.1} {} {}", unit.unit_num, unit.unit_id, unit.unit_name);
-        if &unit.unit_id[..4] == "GB40" || &unit.unit_id[..4] == "GB30" || &unit.unit_id[..4] == "GB20" {
-            countn0 += unit.unit_num;
+    while units.len() > 0 {
+        let unit = units.pop().unwrap();
+        if unit.grade_num > -2.0 {
+            println!("専門    科目: {:>2.1} {:<7} {}", unit.unit_num, unit.unit_id, unit.unit_name);
+            if &unit.unit_id[..4] == "GB40" || &unit.unit_id[..4] == "GB30" || &unit.unit_id[..4] == "GB20" {
+                countn0 += unit.unit_num;
+            } else {
+                countn += unit.unit_num;
+            }
         } else {
-            countn += unit.unit_num;
+            println!("専門    科目: {}         {}", colorize("WIP", 33), unit.unit_name);
         }
-        units.retain(|x| x.unit_id != unit.unit_id);
     }
 
     if countn.min(18.0) + countn0 < 36.0 {
@@ -151,25 +170,27 @@ fn check(user: User) -> i32 {
         println!("GBn + GBn0 = {} + {}{}", countn, countn0, colorize(" >= 36", 42));
     }
 
-    units = check_req(units, b_req, "専門基礎科目");
+    units = check_req(user.units_b, b_req, "専門基礎科目");
     let mut misc: f32 = 0.0;
     let mut cseng: f32 = 0.0;
     let mut gb1: f32 = 0.0;
     let mut ga1: f32 = 0.0;
-    while units.iter().any(|x| x.unit_group == "B" && x.grade_num > -2.0) {
-        let unitsbkt2: Vec<Unit> = units.clone();
-        let unit = unitsbkt2.iter().find(|x| x.unit_group == "B" && x.grade_num > -2.0).unwrap();
-        println!("専門基礎科目: {:>2.1} {} {}", unit.unit_num, unit.unit_id, unit.unit_name);
-        if unit.unit_name == "確率論" || unit.unit_name == "統計学" || unit.unit_name == "数値計算法" || unit.unit_name == "論理と形式化" || unit.unit_name == "電磁気学" || unit.unit_name == "論理システム" || unit.unit_name == "論理システム演習" {
-            misc += unit.unit_num;
-        }else if &unit.unit_id[..3] == "GB1" {
-            gb1 += unit.unit_num;
-        } else if &unit.unit_id[..3] == "GA1" {
-            ga1 += unit.unit_num;
-        } else if &unit.unit_name == "Computer Science in English A" || &unit.unit_name == "Computer Science in English B" {
-            cseng += unit.unit_num;
+    while units.len() > 0 {
+        let unit = units.pop().unwrap();
+        if unit.grade_num > -2.0 {
+            println!("専門基礎科目: {:>2.1} {} {}", unit.unit_num, unit.unit_id, unit.unit_name);
+            if unit.unit_name == "確率論" || unit.unit_name == "統計学" || unit.unit_name == "数値計算法" || unit.unit_name == "論理と形式化" || unit.unit_name == "電磁気学" || unit.unit_name == "論理システム" || unit.unit_name == "論理システム演習" {
+                misc += unit.unit_num;
+            }else if &unit.unit_id[..3] == "GB1" {
+                gb1 += unit.unit_num;
+            } else if &unit.unit_id[..3] == "GA1" {
+                ga1 += unit.unit_num;
+            } else if &unit.unit_name == "Computer Science in English A" || &unit.unit_name == "Computer Science in English B" {
+                cseng += unit.unit_num;
+            }
+        } else {
+            println!("専門基礎科目: {}         {}", colorize("WIP", 33), unit.unit_name);
         }
-        units.retain(|x| x.unit_id != unit.unit_id);
     }
 
     if misc < 10.0 {
@@ -184,7 +205,7 @@ fn check(user: User) -> i32 {
         println!("{} misc:{}, CSEng:{}, GB1:{}, GA1{}", colorize("pass", 42), misc, cseng, gb1, ga1);
     }
 
-    units = check_req(units, c_req, "共通基礎科目");
+    units = check_req(user.units_c, c_req, "共通基礎科目");
 
     let mut pe: f32 = 0.0;
     while units.iter().any(|x| x.unit_name.len() > 6 && x.unit_group == "C" && &x.unit_name[6..12] == "体育" && x.grade_num > -2.0) {
@@ -201,16 +222,18 @@ fn check(user: User) -> i32 {
 
     let mut acfnd: f32 = 0.0;
     let mut arts: f32 = 0.0;
-    while units.iter().any(|x| x.unit_group == "C" && x.grade_num > -2.0) {
-        let unitsbkt2: Vec<Unit> = units.clone();
-        let unit = unitsbkt2.iter().find(|x| x.unit_group == "C" && x.grade_num > -2.0).unwrap();
-        println!("共通基礎科目: {:>2.1} {} {}", unit.unit_num, unit.unit_id, unit.unit_name);
-        if &unit.unit_id[..2] == "12" || &unit.unit_id[..2] == "14" {
-            acfnd += unit.unit_num;
+    while units.len() > 0 {
+        let unit = units.pop().unwrap();
+        if unit.grade_num > -2.0 {
+            println!("共通基礎科目: {:>2.1} {} {}", unit.unit_num, unit.unit_id, unit.unit_name);
+            if &unit.unit_id[..2] == "12" || &unit.unit_id[..2] == "14" {
+                acfnd += unit.unit_num;
+            } else {
+                arts += unit.unit_num;
+            }
         } else {
-            arts += unit.unit_num;
+            println!("共通基礎科目: {}         {}", colorize("WIP", 33), unit.unit_name);
         }
-        units.retain(|x| x.unit_id != unit.unit_id);
     }
 
     if acfnd < 1.0 {
@@ -219,18 +242,21 @@ fn check(user: User) -> i32 {
         println!("{} acfnd:{}", colorize("pass", 32), acfnd);
     }
 
+    units = user.units_c0;
     let mut science: f32 = 0.0;
     let mut not_science: f32 = 0.0;
-    while units.iter().any(|x| x.unit_group == "C0" && x.grade_num > -2.0) {
-        let unitsbkt2: Vec<Unit> = units.clone();
-        let unit = unitsbkt2.iter().find(|x| x.unit_group == "C0" && x.grade_num > -2.0).unwrap();
-        println!("関連基礎科目: {:>2.1} {} {}", unit.unit_num, unit.unit_id, unit.unit_name);
-        if &unit.unit_id[..1] == "E" || &unit.unit_id[..1] == "F" || &unit.unit_id[..2] == "GC" || &unit.unit_id[..2] == "GE" || &unit.unit_id[..1] == "H" {
-            science += unit.unit_num;
+    while units.len() > 0 {
+        let unit = units.pop().unwrap();
+        if unit.grade_num > -2.0 {
+            println!("関連基礎科目: {:>2.1} {} {}", unit.unit_num, unit.unit_id, unit.unit_name);
+            if &unit.unit_id[..1] == "E" || &unit.unit_id[..1] == "F" || &unit.unit_id[..2] == "GC" || &unit.unit_id[..2] == "GE" || &unit.unit_id[..1] == "H" {
+                science += unit.unit_num;
+            } else {
+                not_science += unit.unit_num;
+            }
         } else {
-            not_science += unit.unit_num;
+            println!("関連基礎科目: {}         {}", colorize("WIP", 33), unit.unit_name);
         }
-        units.retain(|x| x.unit_id != unit.unit_id);
     }
 
     if not_science < 6.0 {
