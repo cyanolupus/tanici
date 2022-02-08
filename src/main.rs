1bf3ca5217eb6c9055af40b345a286f1d44c3777
cyanolupus <cyanolupus@gmail.com>
2022-02-08 21:00:47 +0900

    remove lib.rs and add clap crate and imports

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use std::fs;
 use std::env;
 
-mod unit;
+#[macro_use]
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+
+pub mod unit;
 use unit::Unit;
 
-mod requirement;
+pub mod requirement;
 use requirement::Requirement;
 use requirement::RequirementGroup;
 
-mod user;
+pub mod user;
 use user::User;
 
 fn print_cmp(left: f32, right: f32, label: &str) {
@@ -17,60 +21,57 @@ fn print_cmp(left: f32, right: f32, label: &str) {
     println!("{}: {:>4}/{:>2}   {}", if left < right {fail} else {pass}, left, right, label);
 }
 
-fn check(units_a: Vec<Unit>, units_b: Vec<Unit>, units_c: Vec<Unit>, units_c0: Vec<Unit>,) -> (RequirementGroup, RequirementGroup, RequirementGroup, RequirementGroup) {
-    let mut a_reqs = RequirementGroup::comp_reqs("専門    ", vec!["主専攻実験A","主専攻実験B","卒業研究A","卒業研究B","専門語学A","専門語学B"]);
-    a_reqs.add_req(Requirement::req_id("n0", "GB(2|3|4)0", 4, false));
-    a_reqs.add_req(Requirement::req_name("n", ".*", false));
+fn check(units_a: Vec<Unit>, units_b: Vec<Unit>, units_c: Vec<Unit>, units_c0: Vec<Unit>, mut a_reqs: RequirementGroup, mut b_reqs: RequirementGroup, mut c_reqs: RequirementGroup, mut c0_reqs: RequirementGroup,) -> (RequirementGroup, RequirementGroup, RequirementGroup, RequirementGroup) {
     a_reqs.push_units(units_a);
-
-    let mut b_reqs = RequirementGroup::comp_reqs("専門基礎", vec!["線形代数A","線形代数B","微分積分A","微分積分B","情報数学A","専門英語基礎","プログラミング入門","コンピュータとプログラミング","データ構造とアルゴリズム","データ構造とアルゴリズム実験","論理回路","論理回路実験"]);
-    b_reqs.add_req(Requirement::req_name("misc", r"確率論|統計学|数値計算法|論理と形式化|電磁気学|論理システム|論理システム演習", false));
-    b_reqs.add_req(Requirement::req_name("cseng", r"Computer Science in English (A|B)", false));
-    b_reqs.add_req(Requirement::req_id("ga1", "GA1", 3, false));
-    b_reqs.add_req(Requirement::req_id("gb1", "GB1", 3, false));
     b_reqs.push_units(units_b);
-
-    let mut c_reqs = RequirementGroup::comp_reqs("共通基礎", vec!["フレッシュマン・セミナー","学問への誘い","English Reading Skills I","English Reading Skills II","English Presentation Skills I","English Presentation Skills II","情報リテラシー\\(講義\\)","情報リテラシー\\(演習\\)","データサイエンス"]);
-    c_reqs.add_req(Requirement::req_id("pe1", "21", 2, true));
-    c_reqs.add_req(Requirement::req_id("pe2", "22", 2, true));
-    c_reqs.add_req(Requirement::req_id("acfnd", "12|14", 2, false));
-    c_reqs.add_req(Requirement::req_name("arts", ".*", false));
     c_reqs.push_units(units_c);
-
-    let mut c0_reqs = RequirementGroup::new("関連基礎");
-    c0_reqs.add_req(Requirement::req_id("sci", "(E|F|H).|G(C|E)", 2, false));
-    c0_reqs.add_req(Requirement::req_name("nsci", ".*", false));
     c0_reqs.push_units(units_c0);
 
     return (a_reqs, b_reqs, c_reqs, c0_reqs);
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let filename: &str;
-    let mut verbose: bool = false;
-    match args.len() {
-        2 => filename = &args[1],
-        3 => match args[1].as_str() {
-            "--verbose" => {
-                verbose = true; 
-                filename = &args[2];
-            },
-            _ => {
-                eprintln!("Usage error: tanici --option /path/to/file.csv");
-                std::process::exit(1);
-            }
-        },
-        _ => {
-            eprintln!("Usage error: tanici /path/to/file.csv");
+    let app = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(Arg::with_name("csv")
+            .help("CSV file path")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("verbose")
+            .short("v")
+            .long("verbose")
+            .help("print all units"))
+        .arg(Arg::with_name("requirements")
+            .short("i")
+            .long("import")
+            .help("import requirements from yaml file")
+            .takes_value(true)
+            .required(true));
+    
+    let matches = app.get_matches();
+    let csv_path = matches.value_of("csv").unwrap();
+    let yaml_path = matches.value_of("requirements").unwrap();
+    let verbose: bool = matches.is_present("verbose");
+    let mut a_reqs: RequirementGroup;
+    let mut b_reqs: RequirementGroup;
+    let mut c_reqs: RequirementGroup;
+    let mut c0_reqs: RequirementGroup;
+    
+    match fs::read_to_string(yaml_path) {
+        Ok(data) => (a_reqs, b_reqs, c_reqs, c0_reqs) = RequirementGroup::yaml2reqs(&data),
+        Err(e) => {
+            eprintln!("{}", e);
             std::process::exit(1);
         }
     }
-    match fs::read_to_string(filename) {
+
+    match fs::read_to_string(csv_path) {
         Ok(data) => {
             let user = User::new(data);
             println!("start checking your graduation possibility");
-            let (a_reqs, b_reqs, c_reqs, c0_reqs) = check(user.units_a, user.units_b, user.units_c, user.units_c0);
+            (a_reqs, b_reqs, c_reqs, c0_reqs) = check(user.units_a, user.units_b, user.units_c, user.units_c0, a_reqs, b_reqs, c_reqs, c0_reqs);
             a_reqs.print(verbose);
             b_reqs.print(verbose);
             c_reqs.print(verbose);
--- src/requirement.rs
+++ src/requirement.rs
@@ -2,6 +2,9 @@ use regex::Regex;
 use super::unit::Unit;
 use std::collections::HashMap;
 
+extern crate yaml_rust;
+use yaml_rust::{YamlLoader, YamlEmitter};
+
 pub struct Requirement {
     name: String,
     name_reg: Option<Regex>,
@@ -23,13 +26,24 @@ impl Requirement {
         }
     }
 
-    pub fn req_id(name: &str, id: &str, matchlen: u8, is_comp: bool) -> Self {
-        let id_reg = Regex::new(&format!(r"^{}.{{{}}}$", id, 7 - matchlen)).unwrap();
+    pub fn req_id(name: &str, id: &str, is_comp: bool) -> Self {
+        let id_reg = Regex::new(id).unwrap();
         let units: Vec<Unit> = Vec::new();
         Requirement {
             name: name.to_string(),
             name_reg: None,
             id_reg: Some(id_reg),
+            units,
+            is_comp
+        }
+    }
+
+    pub fn req_none(name: &str, is_comp: bool) -> Self {
+        let units: Vec<Unit> = Vec::new();
+        Requirement {
+            name: name.to_string(),
+            name_reg: None,
+            id_reg: None,
             units,
             is_comp
         }
@@ -68,7 +82,7 @@ pub struct RequirementGroup {
 }
 
 impl RequirementGroup {
-    pub fn new(group_name: &str) -> Self {
+    fn new(group_name: &str) -> Self {
         let reqs: Vec<Requirement> = Vec::new();
         let sums: HashMap<String, f32> = HashMap::new();
         RequirementGroup {
@@ -78,19 +92,121 @@ impl RequirementGroup {
         }
     }
 
-    pub fn comp_reqs(group_name: &str, names: Vec<&str>) -> Self {
-        let mut reqs: Vec<Requirement> = Vec::new();
-        let mut sums: HashMap<String, f32> = HashMap::new();
-        for name in names {
-            reqs.push(Requirement::req_name(name, &format!(r"^{}$", name), true));
-            sums.insert(name.to_string(), 0.0);
-        }
-        return RequirementGroup{group_name: group_name.to_string(), reqs, sums};
-    }
-
-    pub fn add_req(&mut self, req: Requirement) {
+    fn add_req(&mut self, req: Requirement) {
         self.sums.insert(req.name.clone(), 0.0);
         self.reqs.push(req);
+    }
+
+    pub fn yaml2reqs(yaml: &str) -> (Self, Self, Self, Self) {
+        match YamlLoader::load_from_str(yaml) {
+            Ok(yaml) => {
+                let mut yaml_vec = &yaml[0]["a_reqs"];
+                let mut a_reqs: RequirementGroup = RequirementGroup::new("専門    ");
+                for req in yaml_vec.as_vec().unwrap() {
+                    let name = req["name"].as_str().unwrap();
+                    let is_comp = req["isCp"].as_bool().unwrap();
+                    let reg = req["reg"].as_str().unwrap();
+                    match req["regtype"].as_str() {
+                        Some("name") => {
+                            let req2 = Requirement::req_name(name, reg, is_comp);
+                            a_reqs.reqs.push(req2);
+                        }
+                        Some("id") => {
+                            let req2 = Requirement::req_id(name, reg, is_comp);
+                            a_reqs.reqs.push(req2);
+                        }
+                        Some("none") => {
+                            let req2 = Requirement::req_none(name, is_comp);
+                            a_reqs.reqs.push(req2);
+                        }
+                        _ => {
+                            println!("error");
+                        }
+                    }
+                }
+
+                let mut yaml_vec = &yaml[0]["b_reqs"];
+                let mut b_reqs: RequirementGroup = RequirementGroup::new("専門基礎");
+                for req in yaml_vec["b_reqs"] {
+                    let name = req["name"].as_str().unwrap();
+                    let is_comp = req["isCp"].as_bool().unwrap();
+                    let reg = req["reg"].as_str().unwrap();
+                    match req["regtype"].as_str() {
+                        Some("name") => {
+                            let req2 = Requirement::req_name(name, reg, is_comp);
+                            b_reqs.reqs.push(req2);
+                        }
+                        Some("id") => {
+                            let req2 = Requirement::req_id(name, reg, is_comp);
+                            b_reqs.reqs.push(req2);
+                        }
+                        Some("none") => {
+                            let req2 = Requirement::req_none(name, is_comp);
+                            b_reqs.reqs.push(req2);
+                        }
+                        _ => {
+                            println!("error");
+                        }
+                    }
+                }
+                
+                let mut yaml_vec = &yaml[0]["c_reqs"];
+                let mut c_reqs: RequirementGroup = RequirementGroup::new("共通基礎");
+                for req in yaml_vec["c_reqs"] {
+                    let name = req["name"].as_str().unwrap();
+                    let is_comp = req["isCp"].as_bool().unwrap();
+                    let reg = req["reg"].as_str().unwrap();
+                    match req["regtype"].as_str() {
+                        Some("name") => {
+                            let req2 = Requirement::req_name(name, reg, is_comp);
+                            c_reqs.reqs.push(req2);
+                        }
+                        Some("id") => {
+                            let req2 = Requirement::req_id(name, reg, is_comp);
+                            c_reqs.reqs.push(req2);
+                        }
+                        Some("none") => {
+                            let req2 = Requirement::req_none(name, is_comp);
+                            c_reqs.reqs.push(req2);
+                        }
+                        _ => {
+                            println!("error");
+                        }
+                    }
+                }
+                
+                let mut yaml_vec = &yaml[0]["c0_reqs"];
+                let mut c0_reqs: RequirementGroup = RequirementGroup::new("関連基礎");
+                for req in yaml_vec["c0_reqs"] {
+                    let name = req["name"].as_str().unwrap();
+                    let is_comp = req["isCp"].as_bool().unwrap();
+                    let reg = req["reg"].as_str().unwrap();
+                    match req["regtype"].as_str() {
+                        Some("name") => {
+                            let req2 = Requirement::req_name(name, reg, is_comp);
+                            c0_reqs.reqs.push(req2);
+                        }
+                        Some("id") => {
+                            let req2 = Requirement::req_id(name, reg, is_comp);
+                            c0_reqs.reqs.push(req2);
+                        }
+                        Some("none") => {
+                            let req2 = Requirement::req_none(name, is_comp);
+                            c0_reqs.reqs.push(req2);
+                        }
+                        _ => {
+                            println!("error");
+                        }
+                    }
+                }
+                
+                return (a_reqs, b_reqs, c_reqs, c0_reqs);
+            }
+            Err(e) => {
+                eprintln!("{}", e);
+                std::process::exit(1);
+            }
+        }
     }
 
     pub fn push_units(&mut self, units: Vec<Unit>) {
